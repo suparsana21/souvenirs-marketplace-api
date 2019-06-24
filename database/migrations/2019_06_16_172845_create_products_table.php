de3aa7264d8461f660827a4cda705e8ea1fc144b
Angga-PC <anggawiguna554@gmail.com>
2019-06-24 23:07:10 +0800

    Add migration

---
+++ database/migrations/2019_06_16_172845_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->uuid('unit_id');
+            $table->uuid('category_id');
+            $table->integer('weight');
+            $table->integer('height');
+            $table->string('thumbnail');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
